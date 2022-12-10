88a68b746cea1b8a0f6112a295cac8e558500059
Jaume Curto Castellví <jaumecurtojob@gmail.com>
2022-12-10 12:55:44 +0100

    Commit About Us

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -7,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  observer: any;
+
+  hiddenElements: any;
+
+  constructor() {}
+
 
   ngOnInit() {
+    this.hiddenElements = document.querySelectorAll('.hidden');
+
+    this.observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('show');
+        } else {
+          entry.target.classList.remove('show');
+        }
+      });
+    });
+
+    this.hiddenElements.forEach((el: any) => this.observer.observe(el));
   }
 
 }
