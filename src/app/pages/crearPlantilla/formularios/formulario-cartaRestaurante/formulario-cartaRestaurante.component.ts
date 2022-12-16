e7fc15079ee47ae7eb0c96b644970e824d29b74a
Jaume Curto Castellví <jaumecurtojob@gmail.com>
2022-12-16 10:56:24 +0100

    Commit arrays

--- src/app/pages/crearPlantilla/formularios/formulario-cartaRestaurante/formulario-cartaRestaurante.component.ts
+++ src/app/pages/crearPlantilla/formularios/formulario-cartaRestaurante/formulario-cartaRestaurante.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
+import { Plato } from 'src/app/models/cartarestaurant.interface';
 import { CartaRestauranteService } from 'src/app/services/cartaRestaurante.service';
 
 @Component({
@@ -8,22 +9,106 @@ import { CartaRestauranteService } from 'src/app/services/cartaRestaurante.servi
   styleUrls: ['./formulario-cartaRestaurante.component.css']
 })
 export class FormularioCartaRestauranteComponent implements OnInit {
-  abajo: boolean = true;
-  nuevoItem: any;
-
-  seccionForm: FormGroup = this.fb.group({
-    nombre: ['']
+  nuevoItem: FormGroup = this.fb.group({
+    nombre: [''],
+    imagen: [],
+    secciones: this.fb.array([]),
+    menus: this.fb.array([])
   });
+
+  // NUEVOS
+  nuevaSeccionForm: FormGroup = this.fb.group({
+    nombre: [''],
+    imagen: [],
+    platosSeccion: this.fb.array([])
+  });
+
+  nuevoMenuForm: FormGroup = this.fb.group({
+    nombre: [''],
+    imagen: [],
+    precio: [''],
+    platosMenu: this.fb.array([])
+  });
+
+  nuevoPlatoForm: FormGroup = this.fb.group({
+    nombre: [''],
+    descripcion: [''],
+    precio: ['']
+  })
+
+  // TODOS
+  seccionesForm: FormGroup = this.fb.group({
+    secciones: this.fb.array([])
+  });
+
+  menusForm: FormGroup = this.fb.group({
+    menus: this.fb.array([])
+  });
+
+  // GETTERS
+
+  get arrSecciones(){
+    return this.seccionesForm.get('secciones') as FormArray;
+  }
+
+  get arrMenus(){
+    return this.menusForm.controls['menus'] as FormArray;
+  }
+
+  get platosSeccion(){
+    let secc = this.nuevaSeccionForm.controls['secciones'] as FormArray;
+    return secc.get('platosSeccion') as FormArray;
+
+  }
+
+  get platosMenu() {
+    return this.menusForm.get('menus')?.get('platosMenu') as FormArray;
+  }
 
   constructor( private fb: FormBuilder, private crService: CartaRestauranteService) { }
 
   ngOnInit() {
   }
 
-  cambiarIcono() {
-    if (this.abajo) {
-      this.abajo = false;
-    } else this.abajo = true;
+  guardarPlatosSeccion(){
+    console.log('1');
+    let plato: Plato = {
+      nombre: this.nuevoPlatoForm.get('nombre')?.value,
+      descripcion: this.nuevoPlatoForm.get('descripcion')?.value,
+      precio: this.nuevoPlatoForm.get('precio')?.value
+    };
+    console.log('2');
+    this.crService.addPlato(plato);
+    console.log('3');
+    // TENDRE QUE HACER OTRO FUNCION PARA GUARDAR PLATOS EN MENU,
+    // AÚN ASÍ, NECESITO PODER DIFERENCIAR ENTRE LOS DIFERENTES
+    // MENUS AL IGUAL QUE LAS SECCIONES
+
+    let parray = this.nuevaSeccionForm.get('platosSeccion') as FormArray;
+    console.log('4');
+    parray.push(
+      this.fb.group({
+        nombre: [plato.nombre],
+        descripcion: [plato.descripcion],
+        precio: [plato.precio]
+      })
+    );
+    console.log('5');
+    //this.platosSeccion.push(this.nuevoPlatoForm);
+
+    this.nuevoPlatoForm.reset();
+  }
+
+  guardarPlatoMenu(){
+
+  }
+
+  guardarSeccion(){
+
+  }
+
+  guardarMenu(){
+
   }
 
   guardarItem() {
@@ -42,9 +127,7 @@ export class FormularioCartaRestauranteComponent implements OnInit {
 
   }
 
-  guardarDatos(){
 
-  }
 
   generarPDF(){
 
