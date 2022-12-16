cf85c389ab2e89cf50706e2315b211a398ac04d2
JuanManuelDS <juanmadesenzi@gmail.com>
2022-12-16 09:11:22 +0100

    plantilla listado responsive

--- src/app/pages/crearPlantilla/plantillas/listado/listado.component.ts
+++ src/app/pages/crearPlantilla/plantillas/listado/listado.component.ts
@@ -31,31 +31,65 @@ export class ListadoComponent implements OnChanges {
   guardarPDF() {
     const data: any = document.getElementById('documento');
     html2canvas(data).then((canvas) => {
-      const imgWidth = 210;
-      const pageHeight = 297;
-      const imgHeight = (canvas.height * imgWidth) / canvas.width;
-      let heightLeft = imgHeight;
-      console.log(heightLeft);
-      let position = 0;
-      heightLeft -= pageHeight;
-      const doc = new jsPDF('p', 'mm', 'a4');
-      doc.addImage(canvas, 'PNG', 0, position, imgWidth, imgHeight, '', 'FAST');
-      while (heightLeft >= 0) {
-        position = heightLeft - imgHeight;
-        doc.addPage();
-        doc.addImage(
-          canvas,
-          'PNG',
-          0,
-          position,
-          imgWidth,
-          imgHeight,
-          '',
-          'FAST'
+      const image = { type: 'jpeg', quality: 0.98 };
+      const margin = [0.5, 0.5];
+      const filename = 'myfile.pdf';
+
+      var imgWidth = 8.5;
+      var pageHeight = 11;
+
+      var innerPageWidth = imgWidth - margin[0] * 2;
+      var innerPageHeight = pageHeight - margin[1] * 2;
+
+      // Calculate the number of pages.
+      var pxFullHeight = canvas.height;
+      var pxPageHeight = Math.floor(canvas.width * (pageHeight / imgWidth));
+      var nPages = Math.ceil(pxFullHeight / pxPageHeight);
+
+      // Define pageHeight separately so it can be trimmed on the final page.
+      var pageHeight = innerPageHeight;
+
+      // Create a one-page canvas to split up the full image.
+      var pageCanvas = document.createElement('canvas');
+      var pageCtx = pageCanvas.getContext('2d');
+      pageCanvas.width = canvas.width;
+      pageCanvas.height = pxPageHeight;
+
+      // Initialize the PDF.
+      var pdf = new jsPDF('p', 'in', [8.5, 11]);
+
+      for (var page = 0; page < nPages; page++) {
+        // Trim the final page to reduce file size.
+        if (page === nPages - 1 && pxFullHeight % pxPageHeight !== 0) {
+          pageCanvas.height = pxFullHeight % pxPageHeight;
+          pageHeight = (pageCanvas.height * innerPageWidth) / pageCanvas.width;
+        }
+
+        // Display the page.
+        var w = pageCanvas.width;
+        var h = pageCanvas.height;
+        pageCtx!.fillStyle = 'white';
+        pageCtx!.fillRect(0, 0, w, h);
+        pageCtx!.drawImage(canvas, 0, page * pxPageHeight, w, h, 0, 0, w, h);
+
+        // Add the page to the PDF.
+        if (page > 0) pdf.addPage();
+        debugger;
+        var imgData = pageCanvas.toDataURL(
+          'image/' + image.type,
+          image.quality
         );
-        heightLeft -= pageHeight;
+        pdf.addImage(
+          imgData,
+          image.type,
+          margin[1],
+          margin[0],
+          innerPageWidth,
+          pageHeight
+        );
       }
-      doc.save('Downld.pdf');
+
+      pdf.save();
     });
   }
 
