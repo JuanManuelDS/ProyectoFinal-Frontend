bbf6b6cece6466f93def77114203c9487361688d
Jaume Curto Castellví <jaumecurtojob@gmail.com>
2022-12-19 17:21:21 +0100

    Delete Plantilla Beta

--- src/app/services/plantillas.service.ts
+++ src/app/services/plantillas.service.ts
@@ -47,6 +47,18 @@ export class PlantillasService {
     return this.http.get<Plantilla>(url, { headers });
   }
 
+  deletePlantilla(id: number) {
+    //Tomo el token del local storage (en caso que lo tenga)
+    const token = 'Bearer ' + localStorage.getItem('token');
+
+    //En caso que no tenga un token en el localstorage paso un string vacío
+    const headers = new HttpHeaders().set('Authorization', token || '');
+
+    const url = `https://proyectofinal-backend-production-8cff.up.railway.app/api/plantillas/${id}`;
+    return this.http.delete<Plantilla>(url, { headers });
+
+  }
+
   get usuario() {
     return this.authService.usuario;
   }
