c010e758b101c66ba6a1340053f2ebb976f4bf5a
Jaume Curto Castellví <jaumecurtojob@gmail.com>
2022-12-21 19:02:39 +0100

    Userpage

--- src/app/components/userpage/userpage.component.ts
+++ src/app/components/userpage/userpage.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-userpage',
@@ -6,10 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./userpage.component.css']
 })
 export class UserpageComponent implements OnInit {
+  username = localStorage.getItem('nombreUsuario');
+  mismatch: boolean = false;
+  passSetted: boolean = false;
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private authServ: AuthService) { }
+
+  passwordForm: FormGroup = this.fb.group({
+    newPassword: '',
+    newPasswordRepeat: ''
+  });
 
   ngOnInit() {
   }
 
+  setNewPassword(){
+    let p1 = this.passwordForm.controls['newPassword'].value;
+    let p2 = this.passwordForm.controls['newPasswordRepeat'].value;
+
+    if(p1.match(p2) && p1 != '' && p2 != '' && p1.length >= 6 && p2.length >= 6){
+      this.mismatch = false;
+      this.authServ.setPassword(p1);
+      this.passwordForm.reset();
+      this.passSetted = true;
+    }else{
+      this.mismatch = true;
+    }
+  }
 }
